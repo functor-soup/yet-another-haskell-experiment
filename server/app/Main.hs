89e5d30552be70805120c94780a551f27d8b433b
luke <nerorevenge@gmail.com>
2017-03-23 10:54:36 -0400

    Server now used sqlite for persistence

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -2,9 +2,12 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 import GHC.Generics
-
 import Data.Aeson
 import Web.Scotty
+import Control.Applicative
+import Control.Monad.IO.Class
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
 
 
 data Todo = Todo { id :: Int, work :: String } deriving (Generic, Show)
@@ -13,17 +16,39 @@ type TodoList = [Todo]
 instance FromJSON Todo
 instance ToJSON Todo
 
+instance FromRow Todo where
+  fromRow = Todo <$> field <*> field
 
-inMemoryStuff :: TodoList
-inMemoryStuff = [Todo 1 "Learn Monad Transformers",
-                 Todo 2 "Learn Lenses",
-                 Todo 3 "Write a bittorrent client from scratch",
-                 Todo 4 "Have a Haskell Adventure"]
+instance ToRow Todo where
+  toRow (Todo id_ str) = toRow (id_, str)
 
 
-routes :: ScottyM ()
-routes = get "/" $ Web.Scotty.json inMemoryStuff
+-- queries
+queryAll :: Query
+queryAll =  "SELECT * from test"
 
+insertInto :: Query
+insertInto = "INSERT INTO test (str) VALUES (?)" 
+
+
+throwDataAtClient :: Connection -> ActionM()
+throwDataAtClient conn = do
+                           rows <- liftIO (query_ conn queryAll) :: ActionM [Todo] 
+                           Web.Scotty.json rows   
+
+
+insertData :: Connection -> ActionM ()
+insertData conn = do
+                     dat <- jsonData :: ActionM Todo
+                     liftIO $ execute conn insertInto $ Only (work dat)
+                     html "Done deal" 
+                    
+
+routes :: Connection -> ScottyM()
+routes conn = get "/" (throwDataAtClient conn) >>
+              post "/post" (insertData conn)
 
 main :: IO ()
-main = scotty 3000 routes 
+main = do
+        conn <- open "test.db"
+        scotty 3000 $ routes conn
---
+++ server/sqliteStuff.sh
@@ -0,0 +1,2 @@
+sqlite3 test.db "CREATE TABLE test (id INTEGER PRIMARY KEY, str text);\
+INSERT INTO test (str) VALUES ('Learn Monad Transformers'),('Learn Lens'),('Learn Wreq'), ('have a Haskell Adventure');"
