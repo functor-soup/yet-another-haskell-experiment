0d51ab56a998b2d21ffcfebabaaa6773ec21cf03
luke <nerorevenge@gmail.com>
2017-03-22 23:43:20 -0400

    Simple server stuff

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,6 +1,29 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-import Lib
+import GHC.Generics
+
+import Data.Aeson
+import Web.Scotty
+
+
+data Todo = Todo { id :: Int, work :: String } deriving (Generic, Show)
+type TodoList = [Todo]
+
+instance FromJSON Todo
+instance ToJSON Todo
+
+
+inMemoryStuff :: TodoList
+inMemoryStuff = [Todo 1 "Learn Monad Transformers",
+                 Todo 2 "Learn Lenses",
+                 Todo 3 "Write a bittorrent client from scratch",
+                 Todo 4 "Have a Haskell Adventure"]
+
+
+routes :: ScottyM ()
+routes = get "/" $ Web.Scotty.json inMemoryStuff
+
 
 main :: IO ()
-main = someFunc
+main = scotty 3000 routes 
