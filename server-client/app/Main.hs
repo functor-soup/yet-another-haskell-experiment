17b3620f9ebbe7616bfbe8c7f9b69fb1d6b3f730
luke <nerorevenge@gmail.com>
2017-04-04 21:48:48 -0400

    server-client servers frontend now

--- server-client/app/Main.hs
+++ server-client/app/Main.hs
@@ -6,11 +6,16 @@ import GHC.Generics
 import Web.Scotty
 import System.Environment
 import System.Random
+import System.Directory
 import qualified Network.Wreq as W
 import Data.Aeson 
 import Control.Monad.IO.Class
 import Control.Lens
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Static
+
+locationOfFrontend :: String
+locationOfFrontend = "/../frontend/dist"
 
 type DataRoute = String
 
@@ -22,7 +27,6 @@ instance FromJSON Todo
 data Todoink = Todoink { job :: String } deriving (Generic, Show)
 
 instance FromJSON Todoink
-
 
 
 clientGet :: DataRoute -> ActionM ()
@@ -44,8 +48,21 @@ clientPost url = do
 
 routes :: DataRoute -> ScottyM()
 routes x = (get "/data" $ clientGet x) >>
-           (post "/data" $ clientPost x)
+           (post "/data" $ clientPost x) 
+
+
+serveFront :: String -> ScottyM()
+serveFront x = get "/" $ file (x ++ locationOfFrontend ++ "/index.html")
+
+
+staticMiddleware :: String -> ScottyM()
+staticMiddleware  x = middleware . staticPolicy . addBase $ x
 
 
 main :: IO ()
-main = scotty 4000 $ (middleware logStdoutDev >> routes "http://localhost:5000")
+main = do
+         cwd <- getCurrentDirectory 
+         scotty 4000 $ (middleware logStdoutDev >> 
+              staticMiddleware (cwd++locationOfFrontend) >>
+              routes "http://localhost:5000" >> 
+              serveFront cwd)
